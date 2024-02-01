30ef3c929b0da3452b30cdf4f9c9a46fa26aba64
koseoglufarukk <koseoglufarukk@gmail.com>
2024-02-01 18:53:02 +0300

    Initial commit

---
+++ src/main/java/com/engman/engman/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.engman.engman;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hey %s!", name);
+    }
+}
---
+++ src/test/java/com/engman/engman/EngmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engman.engman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
