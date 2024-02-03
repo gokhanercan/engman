98026233393d67682a25f6602288bedfeea320dd
Gökhan Ercan <mail@gokhanercan.com>
2024-02-03 11:38:49 +0300

    Added initial timeframe and transferrable concepts.

--- src/main/java/com/engman/domain/Project.java
+++ src/main/java/com/engman/domain/Project.java
@@ -1,14 +1,30 @@
 package com.engman.domain;
 
+import com.engman.balance.Liquid;
+import com.engman.domain.time.TimeFrame;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Project {
     public String Name;
-    public ArrayList<Developer> Developers;
+    public List<Developer> Developers = new ArrayList<>();
 
-    public ArrayList<Skill> SkillsRequired;
+    public List<Skill> SkillsRequired;
 
-    public Project(String name) {
+    public Liquid Balance;
+
+    public Project(String name, Integer initialBudget) {
         Name = name;
+        Balance = new Liquid(initialBudget);
+    }
+
+    public Project Run(TimeFrame tf){
+        //TODO: We need a deep copy here. Take a snapshot of the entire setting.    Prototype pattern.
+        for (int t = 0; t < tf.Ticks; t++) {
+            for (Developer dev : Developers) {
+                Integer newBalance = Balance.Transfer(dev.TickSalary * -1);
+            }
+        }
+        return this;
     }
 }
---
+++ src/test/java/com/engman/domain/ProjectTest.java
@@ -0,0 +1,20 @@
+package com.engman.domain;
+
+import com.engman.domain.time.TimeFrame;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectTest {
+
+    @Test
+    void run() {
+        Project target = new Project("p1",1000);
+        Skill s1 = new Skill("donet");
+        target.Developers.add(new Developer("Dev1", new Skill[]{s1},10));
+        target.Developers.add(new Developer("Dev2", new Skill[]{s1},10));
+        Project actual = target.Run(new TimeFrame(10));
+
+        assertEquals(800,actual.Balance.GetCurrent());
+    }
+}
