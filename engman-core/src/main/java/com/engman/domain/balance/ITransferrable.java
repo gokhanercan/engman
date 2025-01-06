c441efeca3d1ff0779efa12d5b23cb202ec63b25
ali-moj <amojtehed@gmail.com>
2025-01-06 13:14:56 +0330

    init

---
+++ engman-core/src/main/java/com/engman/domain/balance/ITransferrable.java
@@ -0,0 +1,15 @@
+package com.engman.domain.balance;
+
+public interface ITransferrable {
+
+    //On every iteration/hit/scale
+    Integer Transfer(Integer bit);
+    Integer GetCurrent();
+}
+
+/*
+Project     -> Scope+++
+Budget      -> Budget--
+Developer   -> Revenue--
+Skill       ->
+ */
