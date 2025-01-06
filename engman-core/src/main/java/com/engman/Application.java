7bf137b1059f3fabc796c5e44460ad5548078793
ali-moj <amojtehed@gmail.com>
2025-01-06 13:15:34 +0330

    init

--- engman-core/src/main/java/com/engman/Application.java
+++ engman-core/src/main/java/com/engman/Application.java
@@ -1,20 +1,10 @@
 package com.engman;
 
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * The entry point of the Spring Boot application.
- *
- * Use the @PWA annotation make the application installable on phones, tablets
- * and some desktop browsers.
- *
- */
-@SpringBootApplication
-@Theme(value = "eng-man")
-public class Application implements AppShellConfigurator {
+import org.springframework.boot.SpringApplication;
+
+
+public class Application  {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
