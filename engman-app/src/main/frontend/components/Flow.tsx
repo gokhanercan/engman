0009febe40b8a0a7b0b8e48dc37a7ee278faca34
Gökhan Ercan <mail@gokhanercan.com>
2025-01-27 14:22:15 +0300

    Developer-Project group rel.

--- engman-app/src/main/frontend/components/Flow.tsx
+++ engman-app/src/main/frontend/components/Flow.tsx
@@ -18,35 +18,44 @@ export default function Flow({ projects }: FlowProps) {
   //     (params) => setEdges((eds) => addEdge(params, eds)),
   //     [setEdges],
   //   );
+  
   const projectNodes:Node[] = projects.map((project, index) => ({
     id: project.ID?.toString() ?? '',
-    position: { x: 100, y: 75 + index * 100 },
-    data: { label: project.Name ?? '' },
-    parentId: 'G1',
-    extent: 'parent',
+    position: { x: 25, y: 75 + index * 120 },
+    data: { label: project.Name ?? 'n/a' },
+    type:'group',
+    style: { width: 200, height: 100, backgroundColor: 'rgba(39, 91, 233, 0.16)',},
   }));
-  projectNodes.push({ id: 'G1', type:'group',
-    position: { x: 50, y: 50 }, 
-    data: { label: 'G1' },
-    style: { width: 300, height: 300,    backgroundColor: 'rgba(39, 91, 233, 0.16)',},
-    });
-    projectNodes.push({ id: 'PX',
-        position: { x: 100, y: 400 }, 
-        data: { label: 'PX' },
-        });
-  const projectEdges: any = [
-    { id: 'a', source: '1', target: '2', animated: true, style: { stroke: 'red' } },
-    { id: 'b', source: '2', target: '3', animated: true },
-  ];
 
-  const [nodes, setNodes, onNodesChange] = useNodesState(projectNodes);
+  const devNodes:any = projects.flatMap((project) => project.Developers ?? []).map((dev, proDevIndex) => ({ 
+    id: `${proDevIndex}-{dev?.Name ?? ''}`,
+    position: { x: 0, y: 0 + proDevIndex * 40 },
+    data: { label: `${dev?.Name ?? 'n/a'}` },
+    style: { width: 50, height: 30, backgroundColor: 'white',},
+    parentId: '1', extent: 'parent' //TODO: get project id of dev
+  }));
+
+  const allNodes = projectNodes.concat(devNodes);
+
+  // projectNodes.push({ id: 'G1', type:'group',
+  //   position: { x: 50, y: 50 }, 
+  //   data: { label: 'G1' },
+  //   style: { width: 300, height: 300, backgroundColor: 'rgba(39, 91, 233, 0.16)',},
+  //   });
+  
+  // const projectEdges: any = [
+  //   { id: 'a', source: '1', target: '2', animated: true, style: { stroke: 'red' } },
+  //   { id: 'b', source: '2', target: '3', animated: true },
+  // ];
+
+  const [nodes, setNodes, onNodesChange] = useNodesState(allNodes);
 //   const [edges, setEdges, onEdgesChange] = useEdgesState(projectEdges);
 //   const onConnect = useCallback((params: any) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
 
   return (
     <div style={{ height: '100%', border: '1px solid red' }}>
       <ReactFlow
-        nodes={projectNodes}
+        nodes={allNodes}
         // edges={projectEdges}
         onNodesChange={onNodesChange}
         // onEdgesChange={onEdgesChange}
