60a601a558400a08ab4a6ee4aa291f88173dd9c9
Gökhan Ercan <mail@gokhanercan.com>
2025-02-27 14:19:01 +0300

    clean up

--- engman-app/src/main/frontend/components/Developers.tsx
+++ engman-app/src/main/frontend/components/Developers.tsx
@@ -16,31 +16,16 @@ export default function Developers({ developers,title,showProgressBars=true,page
     return (
         <>
             {title && <b className="subTitle">{title}</b>}
-            {/* {pageLink && <a href={pageLink}>Page</a>} */}
-            {/* <a href="dsada">Gokhan</a> */}
-            {/* <Icon icon="vaadin:phone" /> */}
             <Grid items={developers}>
                 <GridColumn
                     header={"Avatar"}
                     renderer={({ item }) => (
                         <>
                             <DeveloperAvatar developer={item} />
-                            {/* <p style={{display:'inline'}}>{item.Name}</p> */}
                         </>
                     )}
                 />
                 <GridColumn path="name" header="Name" />
-                {/* <GridColumn path="Age" header="Age"  /> */}
-                {/* <GridColumn header="Age2"
-                    renderer={({ item }) => (
-                        <>
-                            <span>{item.Age}</span>
-                            {showProgressBars && 
-                                <Progress min={20} max={60} value={item.Age} />
-                            }
-                        </>
-                    )}
-                /> */}
                 <GridColumn
                     header={"Skills"}
                     renderer={({ item }) => (
